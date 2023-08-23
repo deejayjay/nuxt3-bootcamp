263b99ecf3fdc476bf841d6264555741349066bb
Deepak Joy Jose <72120524+deejayjay@users.noreply.github.com>
2023-08-22 20:02:33 -0600

    Adds the hero banner

--- cartrader/app.vue
+++ cartrader/app.vue
@@ -7,5 +7,21 @@
       </h1>
     </header>
     <!-- NAVBAR END -->
+
+    <!-- HOME HERO START -->
+    <div class="hero relative h-screen w-100 bg-no-repeat bg-cover bg-bottom">
+      <div class="bg-black bg-opacity-75 absolute inset-0 flex justify-center items-center">
+        <div class="text-center w-50">
+          <h2 class="text-8xl font-sans mb-10 text-white">Find your drive</h2>
+        </div>
+      </div>
+    </div>
+    <!-- HOME HERO END -->
   </div>
 </template>
+
+<style scoped>
+.hero {
+  background-image: url('./assets/images/hero.jpg');
+}
+</style>
