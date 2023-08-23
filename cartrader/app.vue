e32b70c4cd62abd37388565f77ff2673995dab5c
Deepak Joy Jose <72120524+deejayjay@users.noreply.github.com>
2023-08-22 21:47:12 -0600

    Adds Card Details

--- cartrader/app.vue
+++ cartrader/app.vue
@@ -1,13 +1,5 @@
 <template>
-  <div>
-    <!-- NAVBAR START -->
-    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
-      <h1>
-        <a href="#" class="text-3xl font-mono">Cartrader</a>
-      </h1>
-    </header>
-    <!-- NAVBAR END -->
-  </div>
+  <div></div>
 </template>
 
 <style scoped>
