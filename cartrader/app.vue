bde26b3fa94c834fe8751ef2cfa2bf0319aa0ad4
Deepak Joy Jose <72120524+deejayjay@users.noreply.github.com>
2023-08-22 19:40:05 -0600

    Adds the Navbar.

--- cartrader/app.vue
+++ cartrader/app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- NAVBAR START -->
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <h1>
+        <a href="#" class="text-3xl font-mono">Cartrader</a>
+      </h1>
+    </header>
+    <!-- NAVBAR END -->
   </div>
 </template>
