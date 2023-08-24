7b794fa4889f716b79627b9a76614ebc7d2b4f33
Deepak Joy Jose <72120524+deejayjay@users.noreply.github.com>
2023-08-23 20:49:34 -0600

    Adds the /city/[city]/car/[make] route and adds components for the route

---
+++ cartrader/components/Car/Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+    <div class="flex h-full">
+      <img src="@/assets/images/toyota-truck.jpg" alt="Toyoto Tacoma" class="w-[300px] h-full" />
+      <section class="p-4 flex flex-col">
+        <h2 class="text-2xl text-blue-700">Toyoto Tacoma</h2>
+        <p class="text-gray-700">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        <p class="mt-auto text-3xl font-medium">$79,449</p>
+      </section>
+    </div>
+  </div>
+</template>
---
+++ cartrader/components/Car/SideBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="shadow border w-64 mr-10 z-30 h-[190px]">
+    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <h3 class="font-medium">Location</h3>
+      <p class="text-blue-400 capitalize">Ottawa</p>
+      <!-- <form class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
+          <input type="text" class="border p-1 rounded" />
+          <button type="submit" class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
+        </form> -->
+    </div>
+    <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <h3 class="font-medium">Make</h3>
+      <p class="text-blue-400 capitalize">Any</p>
+    </div>
+    <div class="p-5 flex justify-between relative cursor-pointer">
+      <h3 class="font-medium">Price</h3>
+      <p class="text-blue-400 capitalize">Any</p>
+    </div>
+  </nav>
+</template>
