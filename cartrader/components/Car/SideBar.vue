8024092338a4f42c18a59c0432b6fa77128333a9
DeeJayJay <deejayjay@zoho.com>
2023-08-27 20:02:32 -0600

    Adds the Car Price filter

--- cartrader/components/Car/SideBar.vue
+++ cartrader/components/Car/SideBar.vue
@@ -1,11 +1,26 @@
 <script setup>
 const route = useRoute();
+const router = useRouter();
+
 const modal = ref({
   location: false,
   make: false,
   price: false,
 });
 const city = ref('');
+const priceRange = ref({
+  min: "",
+  max: ""
+});
+
+const priceRangeText = computed(() => {
+  const { minPrice, maxPrice } = route.query;
+
+  if (!minPrice && !maxPrice) return "Any";
+  if (!minPrice && maxPrice) return `Under $${maxPrice}`;
+  if (minPrice && !maxPrice) return `Above $${minPrice}`;
+  return `$${minPrice} - $${maxPrice}`;
+});
 
 const { makes } = useCars();
 
@@ -30,11 +45,24 @@ function onMakeClick(make) {
   modal.value.make = false;
   navigateTo(`/city/${route.params.city}/car/${make}`);
 }
+
+function onPriceSubmit() {
+  if (priceRange.value.min && priceRange.value.max && priceRange.value.min > priceRange.value.max) {
+    return;
+  }
+  updateModal('price');
+  router.push({
+    query: {
+      minPrice: priceRange.value.min,
+      maxPrice: priceRange.value.max
+    },
+  });
+}
 </script>
 
 <template>
   <nav class="shadow border w-96 mr-10 z-30 h-[190px]">
-    <!-- Location starts -->
+    <!-- Location start -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3 class="font-medium">Location</h3>
       <p class="text-blue-400 capitalize"
@@ -49,8 +77,8 @@ function onMakeClick(make) {
                 class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
       </form>
     </div>
-    <!-- Location ends -->
-    <!-- Make starts -->
+    <!-- Location end -->
+    <!-- Make start -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3 class="font-medium">Make</h3>
       <p class="text-blue-400 capitalize"
@@ -64,12 +92,27 @@ function onMakeClick(make) {
                 @click="onMakeClick(make)">{{ make }}</button>
       </div>
     </div>
-    <!-- Make ends -->
-    <!-- Price starts -->
+    <!-- Make end -->
+    <!-- Price start -->
     <div class="p-5 flex justify-between relative cursor-pointer">
       <h3 class="font-medium">Price</h3>
-      <p class="text-blue-400 capitalize">Any</p>
+      <p class="text-blue-400 capitalize"
+         @click="updateModal('price')">{{ priceRangeText }}</p>
+      <form v-if="modal.price"
+            @submit.prevent="onPriceSubmit"
+            class="absolute left-56 top-1 border shadow p-5 -m-1 bg-white">
+        <input type="number"
+               placeholder="Min"
+               v-model="priceRange.min"
+               class="border p-1 rounded mb-2">
+        <input type="number"
+               placeholder="Max"
+               v-model="priceRange.max"
+               class="border p-1 rounded mb-2">
+        <button type="submit"
+                class="bg-blue-400 w-full rounded text-white p-1">Apply</button>
+      </form>
     </div>
-    <!-- Price ends -->
+    <!-- Price end -->
   </nav>
 </template>
