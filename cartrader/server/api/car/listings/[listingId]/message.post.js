21b5b56995d5fbd351a8df634db072ecdf08ef5e
DeeJayJay <deejayjay@zoho.com>
2023-09-15 22:52:51 -0600

    Adds all endpoints required in the app.

---
+++ cartrader/server/api/car/listings/[listingId]/message.post.js
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import Joi from "joi";
+
+const schema = Joi.object({
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] }
+    })
+    .required(),
+  phone: Joi.string()
+    .length(10)
+    .pattern(/^[0-9]+$/)
+    .required(),
+  name: Joi.string().required(),
+  message: Joi.string().min(20).required()
+});
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const { listingId } = event.context.params;
+
+  const { error } = await schema.validate(body);
+
+  if (error) {
+    throw createError({
+      statusCode: 412,
+      message: error.message
+    });
+  }
+
+  return await prisma.message.create({
+    data: {
+      ...body,
+      listingId: parseInt(listingId)
+    }
+  });
+});
---
+++ cartrader/server/api/car/listings/index.post.js
@@ -0,0 +1,69 @@
+import Joi from "joi";
+import { PrismaClient } from "@prisma/client";
+
+const schema = Joi.object({
+  make: Joi.string().required(),
+  model: Joi.string().required(),
+  year: Joi.number()
+    .required()
+    .min(1900)
+    .max(new Date().getFullYear() + 1),
+  miles: Joi.number().required().min(0),
+  city: Joi.string().required().min(2),
+  numberOfSeats: Joi.number().required().min(1).max(100),
+  description: Joi.string().required().min(20),
+  features: Joi.array().items(Joi.string()).required(),
+  image: Joi.string().required(),
+  listerId: Joi.string().required(),
+  price: Joi.number().required().min(0),
+  name: Joi.string().required()
+});
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  const { error } = await schema.validate(body);
+
+  if (error) {
+    throw createError({
+      statusCode: 412,
+      message: error.message
+    });
+  }
+
+  const {
+    make,
+    model,
+    year,
+    miles,
+    city,
+    numberOfSeats,
+    description,
+    features,
+    image,
+    listerId,
+    price,
+    name
+  } = body;
+
+  const car = await prisma.car.create({
+    data: {
+      make,
+      model,
+      year,
+      miles,
+      city: city.toLowerCase(),
+      numberOfSeats,
+      description,
+      features,
+      image,
+      listerId,
+      price,
+      name
+    }
+  });
+
+  return car;
+});
